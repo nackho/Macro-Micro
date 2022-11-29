4fe5a72e89e50afbbe7c9172cd0df5f43093f331
nackho <nick_ho1011@yahoo.com>
2022-11-29 14:56:33 -0800

    editted crud-helper

--- crud-helper.js
+++ crud-helper.js
@@ -4,10 +4,10 @@ require('./config/database');
 
 // Require the Mongoose models
 const User = require('./models/user');
-// const Item = require('./models/item');
-// const Category = require('./models/category');
+const Bodypart = require('./models/bodypart');
+const Split = require('./models/split');
 // const Order = require('./models/order');
 
 // Local variables will come in handy for holding retrieved documents
-let user, item, category, order;
-let users, items, categories, orders;
+let user, bodypart, split, order;
+let users, bodyparts, splits, orders;
